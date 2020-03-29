2b84ba47ce8075241a11566047fcebfde6375f3e
gianmarco <g.m.pal95@gmail.com>
2020-03-29 11:17:17 +0200

    Dashboard progress

--- results/Shiny_App_Covid/app.R
+++ results/Shiny_App_Covid/app.R
@@ -1,81 +1,227 @@
-#
-# This is a Shiny web application. You can run the application by clicking
-# the 'Run App' button above.
-#
-# Find out more about building applications with Shiny here:
-#
-#    http://shiny.rstudio.com/
-#
-
+## app.R ##
+library(dplyr)
 library(shiny)
-library(ggplot2)
-
-###todo -> manage exceptions in country/dataset combination 
+library(shinydashboard)
 
 
-confirmed = read.csv("data/time_series_covid19_confirmed_global.csv")
-deaths = read.csv("data/time_series_covid19_deaths_global.csv")
-recovered = read.csv("data/time_series_covid19_recovered_global.csv")
+##Download and Update Data##
+url_data_info <- "https://github.com/TheoVerhelst/ulb-mlg-time-series-impact/blob/master/data/COVID19_Country_Info.Rdata?raw=true"
+url_data_global <- "https://github.com/TheoVerhelst/ulb-mlg-time-series-impact/blob/master/data/COVID19_Global_Italy.Rdata?raw=true"
+url_data_global_wgrowth <- "https://github.com/TheoVerhelst/ulb-mlg-time-series-impact/blob/master/data/COVID19_Global_Italy_wGrowth.Rdata?raw=true"
+download.file(url_data_global, destfile= "./COVID19_Global_Italy.Rdata", mode = "wb")
+download.file(url_data_info, destfile= "./COVID19_Country_Info.Rdata", mode = "wb")
+download.file(url_data_global_wgrowth, destfile= "./COVID19_Global_Italy_wGrowth.Rdata", mode = "wb")
+
+info_data <- readRDS("./COVID19_Country_Info.Rdata")
+global_data <- readRDS("./COVID19_Global_Italy_wGrowth.Rdata")
 
 
-#The combination of Country and Province creates UNIQUE values for COUNTRIES SELECTION
-confirmed <- within(confirmed,  Country.Region <- paste(Country.Region, Province.State, sep=" "))
-deaths <- within(deaths,  Country.Region <- paste(Country.Region, Province.State, sep=" "))
-recovered <- within(recovered,  Country.Region <- paste(Country.Region, Province.State, sep=" "))
 
-ui <- fluidPage(
-   
-   # Application title
-   titlePanel("Covid19"),
-   
-   # Sidebar with a slider input for number of bins 
-   sidebarLayout(
-      sidebarPanel(
-        
-         selectInput(inputId = "country",
-                    label = "Choose a country:",
-                    choices = confirmed["Country.Region"]), 
-        
-         selectInput(inputId = "dataset",
-                    label = "Choose a dataset:",
-                    choices = c("Confirmed", "Deaths", "Recovered")),
-        
-         sliderInput("range", 
-                     label = "Days of interest:",
-                     min = -60, max = 0, value = c(-60, 0))),
-         
-         # textInput("country", "Country", "Write the country of interest"),
-         
-      
-      # Show a plot of the generated distribution
-      mainPanel(
-         plotOutput("distPlot")
-      )
-   )
-)
+##Split between Italian and Global Data##
+italian_data <- global_data[(global_data$Country.Region == "Italy") & (global_data$Province.State!="") ,]
+global_data <- setdiff(global_data,italian_data)
 
-# Define server logic required to draw a histogram
-server <- function(input, output) {
+
+
+##Make lists for input panels##
+countries <- unique(global_data["Country.Region"])
+countries_with_regions <- unique(global_data[global_data$Province.State != "","Country.Region" ])
+statistics_italy <- names(global_data)[6:15]
+statistics_global <- names(global_data)[6:8]
+italian_regions <- unique(italian_data["Province.State"])
+
+
+
+
+ui <- dashboardPage(
   
-    datasetInput <- reactive({
-      switch(input$dataset,
-             "Confirmed" = confirmed,
-             "Deaths" = deaths,
-             "Recovered" = recovered)
-    })
+  dashboardHeader(title = "CODE VS COVID19"),
+    dashboardSidebar(
+      sidebarMenu(
+        
+          menuItem("World", tabName = "world", icon = icon("bar-chart-o"),
+                   
+                   selectInput(inputId = "country",
+                               label = "Choose a country:",
+                               selected = countries[0],
+                               choices = countries), 
+                   
+                   
+                   uiOutput("region_selector"),
+                   
+    
+                   
+                   sliderInput("range", 
+                               label = "Days of interest (0 = last day):",
+                               min = -60, max = 0, value = c(-7, 0))
+                   ),
+          
+        
+          menuItem("Italy",tabName = "italy", icon = icon("bar-chart-o"),
+                 
+                  selectInput(inputId = "italy_region",
+                             label = "Choose a region:",
+                             selected = italian_regions[0],
+                             choices = italian_regions), 
+                  
+                  selectInput(inputId = "italy_statistics",
+                              label = "Choose a statistic:",
+                              selected = statistics_italy[0],
+                              choices = statistics_italy), 
+                  
+                  sliderInput("range", 
+                              label = "Days of interest (0 = last day):",
+                              min = -60, max = 0, value = c(-7, 0))
+                
+        )
+      ) 
+        
+    ),
+    
+
+    
+
+  ## Body content
+  dashboardBody(
+    
+    column(width = 4,
+           box(
+             title = "Confirmed", width = NULL, solidHeader = TRUE, status = "primary",
+             plotOutput("confirmed_plot")
+           ),
+           box(
+             title = "Confirmed Growth Rate", width = NULL, solidHeader = TRUE, status = "primary",
+             plotOutput("confirmed_growth_plot")
+           )
+    ),
+    
+    column(width = 4,
+           box(
+             title = "Recovered", width = NULL,solidHeader = TRUE, status = "primary",
+             plotOutput("recovered_plot")
+           ),
+           box(
+             title = "Recovered Growth Rate", width = NULL, solidHeader = TRUE, status = "primary",
+             plotOutput("recovered_growth_plot")
+           )
+    ),
+    
+    column(width = 4,
+           box(
+             title = "Deaths", width = NULL, solidHeader = TRUE,status = "primary",
+             plotOutput("deaths_plot")
+           ),
+           box(
+             title = "Deaths Growth Rate", width = NULL, solidHeader = TRUE, status = "primary",
+             plotOutput("deaths_growth_plot")
+           )
+    )
     
    
-    output$distPlot <- renderPlot({
-      dataset <- datasetInput()
+)
+)
 
-      min = input$range[1]
-      max = input$range[2]
-      today = ncol(dataset)
-      part  <- dataset[dataset$Country.Region == input$country,(today + min): (today +max)]
-      plot(t(as.matrix(part)))
-   })
+
+
+
+server <- function(input, output) {
+  
+  
+  #slice exact part of dataset
+  datasetInput <- reactive({
+    min_date = Sys.Date()+input$range[1]
+    max_date = Sys.Date()+input$range[2]
+
+    
+    if (!is.null(input$region)) {
+      #we assume Region names are unique
+      return(global_data[(global_data$Province.State == input$region) & (global_data$Date >= min_date) & (global_data$Date <= max_date), ]  )
+          
+    } else {
+      
+      return(global_data[(global_data$Country.Region == input$country) & (global_data$Province.State == "") & (global_data$Date >= min_date) & (global_data$Date <= max_date), ])
+      
+    }
+    
+  })
+  
+  ## Render the region selector if a country with regions is chosen
+  output$region_selector <- renderUI({
+    
+    if (input$country %in% countries_with_regions) {
+      selectInput(inputId = 'region',
+                  label = 'Choose a region:',
+                  choices = unique(global_data[global_data$Country.Region == input$country ,"Province.State"])
+                  )
+    } else {
+      return(NULL)
+    }
+    
+  })
+  
+  
+  ########
+  ##PLOTS#
+  ########
+  
+  output$confirmed_plot <- renderPlot({
+    dataset <- datasetInput()
+    plot(x = dataset[,"Date"] , 
+         y = dataset[,"Confirmed",],
+         xlab = "Day",
+         ylab = "Confirmed")
+    
+  })
+  
+  output$recovered_plot <- renderPlot({
+    dataset <- datasetInput()
+    plot(x = dataset[,"Date"] , 
+         y = dataset[,"Recovered",],
+         xlab = "Day",
+         ylab = "Recovered")
+    
+  })
+  
+  output$deaths_plot <- renderPlot({
+    dataset <- datasetInput()
+    plot(x = dataset[,"Date"] , 
+         y = dataset[,"Deaths",],
+         xlab = "Day",
+         ylab = "Deaths")
+    
+  })
+  
+  output$confirmed_growth_plot <- renderPlot({
+    dataset <- datasetInput()
+    plot(x = dataset[,"Date"] , 
+         y = dataset[,"ConfirmedGrowthRate",],
+         xlab = "Day",
+         ylab = "Confirmed Growth Rate")
+    
+  })
+  
+  output$recovered_growth_plot <- renderPlot({
+    dataset <- datasetInput()
+    plot(x = dataset[,"Date"] , 
+         y = dataset[,"RecoveredGrowthRate",],
+         xlab = "Day",
+         ylab = "Recovered Growth Rate")
+    
+  })
+  
+  output$deaths_growth_plot <- renderPlot({
+    dataset <- datasetInput()
+    plot(x = dataset[,"Date"] , 
+         y = dataset[,"DeathsGrowthRate",],
+         xlab = "Day",
+         ylab = "Deaths Growth Rate")
+    
+  })
+  
+  
 }
 
-# Run the application 
-shinyApp(ui = ui, server = server)
+
+
+shinyApp(ui, server)
 
